ee1f2d2b6639b67b59fa02bd40e6e5a2f1fcf71c
Janay Bailey <tjanay87@gmail.com>
2019-10-28 20:23:37 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,18 @@
 require 'pry'
 require 'environment.rb'
-require ''
+
+class Song
+
+  attr_accessor :name, :artist, :genre
+
+  @@all = []
+
+  def initialize(name,artist,genre)
+    @name = name
+    @genre = genre
+    @artist = artist
+    @@all << self
+  end
+  def self.all
+    @@all
+  end
